2aff5cf50d51578ac7f44399b710d4cdec81c72e
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-03 23:29:13 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <Button variant="destructive" size="lg">
+      Button
+    </Button>
+  );
+}
+
+export default App;
