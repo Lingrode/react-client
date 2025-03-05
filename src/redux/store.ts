088f5afcc1b22725e5735095d0aabbbbd730ea81
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-04 19:52:41 -0700

    add userApi slice with RTK Query endpoints

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
+import { api } from "@/redux/apis/api";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
