19c779b73c93df9c20a06737307c3accebcbd3ff
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-16 23:12:38 -0600

    followers page

--- src/pages/Followers.tsx
+++ src/pages/Followers.tsx
@@ -1,3 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { User } from "@/components/User";
+import { useAppSelector } from "@/redux/hooks";
+import { selectCurrent } from "@/redux/user/selectors";
+import { Link } from "react-router";
+
 export const Followers = () => {
-  return <div>Followers</div>;
+  const currentUser = useAppSelector(selectCurrent);
+
+  if (!currentUser) return null;
+
+  return currentUser.followers.length > 0 ? (
+    <div className="gap-5 flex flex-col">
+      {currentUser.followers.map((user) => (
+        <Link to={`/users/${user.followerId}`} key={user.follower.id}>
+          <Card>
+            <CardContent>
+              <User
+                name={user.follower.name ?? ""}
+                avatarUrl={user.follower.avatarUrl ?? ""}
+                description={user.follower.email ?? ""}
+              />
+            </CardContent>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  ) : (
+    <h1>You don't have followers</h1>
+  );
 };
